d83518056aa83477b4ce475d45e636ebd42b4827
adeebmuhammed <muhammedadeeb445@gmail.com>
2024-06-11 14:36:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'block'
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
